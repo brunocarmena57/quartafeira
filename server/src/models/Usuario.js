491447629717b29ee1ff79d9b8810865d0589c95
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-04-12 12:04:41 -0300

    Add files via upload

--- server/src/models/Usuario.js
+++ server/src/models/Usuario.js
@@ -3,6 +3,7 @@ import mongoose, { mongo } from "mongoose";
 const UsuarioSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    savedRecipes: [{type: mongoose.Schema.Types.ObjectId, ref: "recipes" }],
 });
 
 export const UserModel = mongoose.model("usuarios", UsuarioSchema)
--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from '../models/Usuario.js';
+import { verify } from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -11,6 +12,49 @@ router.get("/", async (req, res) => {
         res.json(response);
     } catch (err) {
         res.json(err);
+    }
+});
+
+router.post("/", verifyToken, async (req, res) => {
+    const recipe = new RecipeModel (req.body);
+    try {
+        const response = await recipe.save();
+        res.json(response);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.post("/", verifyToken, async (req, res) => {
+    try {
+        const recipe = await RecipeModel.findById(req.body.recipeID);
+        const user = await UserModel.findById(req.body.userID);
+        user.savedRecipes.push(recipe);
+        await user.save();
+        res.json({ savedRecipes: user.savedRecipes });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.get("/savedRecipes/ids/:userID", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.params.userID);
+        res.json({savedRecipes: user?.savedRecipes});
+    } catch (err) {
+        res.json(err)
+    }
+});
+
+router.get("/savedRecipes/:userID", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.params.userID);
+        const savedRecipes = await RecipeModel.find({
+            _id: { $in: user.savedRecipes },
+        });
+        res.json({savedRecipes});
+    } catch (err) {
+        res.json(err)
     }
 });
 
