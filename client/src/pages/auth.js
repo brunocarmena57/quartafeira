6f9aad5a7bdcdd02c93fe7cf0e0d92c9aaf8baf8
brunocarmena57 <gastondectba@hotmail.com>
2023-04-12 00:57:05 -0300

    first update

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom'
 
 export const Auth = () => {
     
@@ -14,8 +17,27 @@ const Login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const [_, setCookies] = useCookies(["access_token"])
+
+    const navigate = useNavigate()
+
+    const onSubmit =  async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:3002/auth/login", {
+                username,
+                password,
+            });
+
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID); 
+            navigate("/");
+        }catch (err) {
+            console.error(err)
+        }
+    }
     return (
-        <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Login"/>
+        <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Login" onSubmit={onSubmit}/>
         );
 }
 
@@ -23,15 +45,29 @@ const Register = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("http://localhost:3002/auth/register", {
+                username,
+                password,
+            });
+            alert("Cadastro completo! Agora faça o seu login.");
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
     return (
-    <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Cadastre-se já!"/>
+    <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Cadastre-se já!" onSubmit={onSubmit}/>
     );
 };
 
-const Form = ({username, setUsername, password, setPassword, label}) => {
+const Form = ({username, setUsername, password, setPassword, label, onSubmit }) => {
     return (
     <div className="auth-container">
-    <form>
+    <form onSubmit={onSubmit}>
         <h2>{label}</h2>
         <div className="form-group">
             <label htmlFor="username">Usuário: </label>
@@ -39,7 +75,7 @@ const Form = ({username, setUsername, password, setPassword, label}) => {
         </div>
         <div className="form-group">
             <label htmlFor="password">Senha: </label>
-            <input type="text" id="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+            <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
         </div>
 
         <button type="submit">
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 import {userRouter} from './routes/usuario.js';
+import {recipesRouter} from './routes/recipes.js';
 
 const app = express()
 
@@ -10,7 +11,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
-mongoose.connect("mongodb+srv://brunocarmena:senha123@receitasapp2.w9qdgt9.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://brunocarmena:senha123@receitasapp2.w9qdgt9.mongodb.net/receitasapp2", {
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(3002, () => console.log("Servidor inicializado!"));
