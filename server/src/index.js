e2cfa832e3496b0da5d72f9cf3c230efa47a41c7
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-04-21 02:18:40 -0300

    Actualizar index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+
 import {userRouter} from './routes/user.js';
 import {recipesRouter} from './routes/recipes.js';
 
